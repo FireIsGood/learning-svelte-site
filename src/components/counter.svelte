9aed5b0a32d2f52f89d33c1e1c2a3a939485ca7c
FireIsGood <fireisadequate@gmail.com>
2022-10-07 14:59:58 -0700

    Add functionality to Count page

--- src/components/counter.svelte
+++ src/components/counter.svelte
@@ -1,1 +1,27 @@
-<p>hello it's me I'm counter I count</p>
+<script>
+  let count = 0;
+
+  function addCount() {
+    count += 1;
+  }
+  function subtractCount() {
+    count -= 1;
+  }
+</script>
+
+<div class="container">
+  <div>
+    <button on:click={addCount}>Add 1</button>
+    <button on:click={subtractCount}>Subtract 1</button>
+  </div>
+  <p>Count is: {count}</p>
+</div>
+
+<style lang="scss">
+  .container > * + * {
+    margin-top: 1rem;
+  }
+  button {
+    padding: 4px;
+  }
+</style>
