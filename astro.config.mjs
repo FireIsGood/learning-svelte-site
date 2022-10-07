45671682f3ac817b4c1d877e5f8c31c07fe2df20
FireIsGood <fireisadequate@gmail.com>
2022-10-07 14:29:22 -0700

    Add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  outDir: "./docs",
+  site: "https://fireisgood.github.io/",
+  base: "/learning-svelte",
 });
