3400a8209d26d4345557324abf6fdee1c4ea9b71
FireIsGood <fireisadequate@gmail.com>
2022-10-07 15:39:20 -0700

    Add reactive statement examples

--- src/components/counter.svelte
+++ src/components/counter.svelte
@@ -1,6 +1,11 @@
 <script>
   let count = 0;
 
+  $: double = 2 * count;
+  $: if (count >= 10) {
+    console.log(`count is ${count} and that's REALLY BIG`);
+  }
+
   function addCount() {
     count += 1;
   }
@@ -14,7 +19,7 @@
     <button on:click={addCount}>Add 1</button>
     <button on:click={subtractCount}>Subtract 1</button>
   </div>
-  <p>Count is: {count}</p>
+  <p>Count is: {count}, double of which is {double}</p>
 </div>
 
 <style lang="scss">
