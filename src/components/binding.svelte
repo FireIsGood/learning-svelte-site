ab494c45fa0419b3ac606ba22658a5e134e2b6a6
FireIsGood <fireisadequate@gmail.com>
2022-10-07 18:39:17 -0700

    Create Binding page

---
+++ src/components/binding.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  let name: string = "mom";
+  let checked: boolean = false;
+</script>
+
+<div class="container">
+  <div class="input-item">
+    <input type="text" name="greet" bind:value={name} />
+    <label for="greet">Hi {name ? name : "no one in particular"}!</label>
+  </div>
+  <div class="input-item">
+    <input type="checkbox" name="stupid" id="stupid" bind:checked />
+    <label for="stupid">{checked}</label>
+  </div>
+</div>
+
+<style lang="scss">
+  .container > * + * {
+    margin-top: 1rem;
+  }
+  .input-item {
+    display: flex;
+  }
+  input[type="checkbox"],
+  input[type="checkbox"] + label {
+    cursor: pointer;
+  }
+</style>
