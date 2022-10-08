17dec93e7fb0b1d338e25da14b285d68c7e423cb
FireIsGood <fireisadequate@gmail.com>
2022-10-07 20:55:11 -0700

    Add more content

--- src/components/binding.svelte
+++ src/components/binding.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   let name: string = "mom";
   let checked: boolean = false;
+  let skillFactor: number = 1;
+  const possibleSkill = ["Typing", "Eating", "Sleeping"];
+  let specificSkill: Array<string> = [];
 </script>
 
 <div class="container">
@@ -9,9 +12,68 @@
     <label for="greet">Hi {name ? name : "no one in particular"}!</label>
   </div>
   <div class="input-item">
-    <input type="checkbox" name="stupid" id="stupid" bind:checked />
-    <label for="stupid">{checked}</label>
+    <input type="checkbox" name="skill" id="skill" bind:checked />
+    <label for="skill">{checked ? "I am VERY skill" : "I am not skill"}</label>
   </div>
+  {#if checked}
+    <div class="input-item">
+      <label>
+        <input
+          type="radio"
+          name="skill-factor"
+          bind:group={skillFactor}
+          value={1}
+        />
+        Tier one skill
+      </label>
+    </div>
+    <div class="input-item">
+      <label>
+        <input
+          type="radio"
+          name="skill-factor"
+          bind:group={skillFactor}
+          value={2}
+        />
+        Tier two skill
+      </label>
+    </div>
+    <div class="input-item">
+      <label>
+        <input
+          type="radio"
+          name="silly-factor"
+          bind:group={skillFactor}
+          value={3}
+        />
+        Tier three skill
+      </label>
+    </div>
+    {#each possibleSkill as skillCheckbox}
+      <div class="input-item">
+        <input
+          type="checkbox"
+          name={skillCheckbox}
+          id={skillCheckbox}
+          bind:group={specificSkill}
+          value={skillCheckbox}
+        />
+        <label for={skillCheckbox}>{skillCheckbox}</label>
+      </div>
+    {/each}
+    {#if skillFactor}
+      <p>
+        Your skill level is {skillFactor >= 3 ? "super high!" : skillFactor}
+      </p>
+    {/if}
+    {#if specificSkill.length > 0}
+      <p>
+        And your skills are: {specificSkill.length === possibleSkill.length
+          ? "Everything!"
+          : specificSkill.join(" ")}
+      </p>
+    {/if}
+  {/if}
 </div>
 
 <style lang="scss">
