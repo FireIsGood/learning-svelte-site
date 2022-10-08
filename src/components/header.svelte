5a6de5fca3d79e1d66d3d0bbc7c0994a091d0324
FireIsGood <fireisadequate@gmail.com>
2022-10-07 20:14:02 -0700

    Make header toggle-able

---
+++ src/components/header.svelte
@@ -0,0 +1,62 @@
+<script>
+  let showLinks = true;
+  function toggleLinks() {
+    showLinks = !showLinks;
+  }
+</script>
+
+<header>
+  <div class="header-main">
+    <div>
+      <slot name="header-main">Header</slot>
+    </div>
+    <button on:click={toggleLinks}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="40"
+        height="40"
+        fill="currentColor"
+        class="bi bi-list"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </button>
+  </div>
+  {#if showLinks}
+    <div>
+      <slot name="header-links" />
+    </div>
+  {/if}
+</header>
+
+<style lang="scss">
+  .header-main {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-inline: 1rem;
+    height: 4rem;
+    background-color: var(--theme-header);
+  }
+
+  button {
+    padding: 8px;
+    border: none;
+    background-color: transparent;
+
+    &:hover {
+      background-color: var(--theme-header-button-hover);
+    }
+    &:active {
+      background-color: var(--theme-header-button-active);
+    }
+
+    svg {
+      display: block;
+    }
+  }
+</style>
