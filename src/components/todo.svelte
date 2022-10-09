fd4bd6e2a27b4b9186fb47df57a54c2810fbcfea
FireIsGood <fireisadequate@gmail.com>
2022-10-08 22:54:16 -0700

    Add localStorage switch

--- src/components/todo.svelte
+++ src/components/todo.svelte
@@ -1,28 +1,78 @@
 <script lang="ts">
+  let useLocalStorage: boolean = false;
+
   type TodoItem = {
     done: boolean;
     task: string;
   };
 
-  let todoItems: Array<TodoItem> = [
+  let todoItems: Array<TodoItem> = [];
+  let sessionItems: Array<TodoItem> = [
     { done: false, task: "Eat Breakfast" },
     { done: false, task: "Refill Water" },
     { done: false, task: "Fight God" },
   ];
 
+  todoItems = sessionItems;
+
+  // LS interaction
+
+  $: {
+    todoItems;
+    if (useLocalStorage) {
+      updateLS();
+    }
+  }
+
+  function toggleLS() {
+    useLocalStorage = !useLocalStorage;
+    if (useLocalStorage) {
+      todoItems = getItems();
+    } else {
+      todoItems = sessionItems;
+    }
+  }
+
+  function getItems() {
+    let LS = localStorage.getItem("todo");
+    if (LS === null || LS === undefined || LS.toString() === "undefined")
+      return [];
+    let localStorageItems = JSON.parse(LS);
+    return localStorageItems;
+  }
+
+  function updateLS() {
+    localStorage.setItem("todo", JSON.stringify(todoItems));
+    console.log("wow");
+  }
+
+  // Button Functions
+
   function addItem() {
     todoItems = [...todoItems, { done: false, task: "" }];
+    updateLS();
   }
 
   function clearItems() {
     todoItems = todoItems.filter((item) => !item.done);
+    updateLS();
   }
 </script>
 
 <div class="container">
-  <div>
+  <div class="buttons">
     <button on:click={addItem}>Add Item</button>
     <button on:click={clearItems}>Clear Completed Items</button>
+    <label>
+      <input
+        type="checkbox"
+        class="local-storage-toggle"
+        on:click={toggleLS}
+        aria-label="Use Local Storage"
+      />
+      Use Local Storage
+    </label>
+    <p>{useLocalStorage}</p>
   </div>
   <ul class="todo-list">
     {#each todoItems as item}
@@ -57,6 +107,26 @@
   .container > * + * {
     margin-top: 1rem;
   }
+
+  .buttons {
+    display: flex;
+    gap: 0.25rem;
+
+    & *:nth-child(2) {
+      margin-right: auto;
+    }
+
+    label {
+      display: flex;
+      align-items: center;
+      font-size: 0.75em;
+
+      .local-storage-toggle {
+        --checkbox-size: 1rem;
+      }
+    }
+  }
+
   .todo-list {
     display: flex;
     flex-direction: column;
