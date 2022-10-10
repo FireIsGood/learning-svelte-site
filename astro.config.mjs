79f57219496dd0e55e54c112a53efaa46d62927a
FireIsGood <fireisadequate@gmail.com>
2022-10-10 08:37:32 -0700

    Install Astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   outDir: "./docs",
   site: "https://fireisgood.github.io/",
   base: "/learning-svelte-site",
