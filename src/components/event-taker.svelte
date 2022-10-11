f63c502b97ab6db4d59bb5cee93bf8ac3dca09ba
FireIsGood <fireisadequate@gmail.com>
2022-10-10 17:07:50 -0700

    Add event page

---
+++ src/components/event-taker.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import { search, hideIncorrect } from "@components/event-store";
+  export let callsign: Props;
+
+  type Props = {
+    text: string;
+    key: string;
+    image: string;
+  };
+
+  let matches: boolean;
+  let exactMatch: boolean;
+
+  $: {
+    const s = $search;
+    if (s) {
+      matches = s.slice(0, s.length) === callsign.key.slice(0, s.length);
+    }
+    exactMatch = s === callsign.key ? true : false;
+  }
+</script>
+
+<div
+  class="container"
+  class:matches
+  class:exactMatch
+  class:fails={!matches &&
+    $search.length > 0 &&
+    $search !== "?" &&
+    $hideIncorrect}
+>
+  <p>{callsign.text}</p>
+  <div class="slot">
+    <slot />
+  </div>
+  {#if exactMatch}
+    <p>Woah that's me</p>
+  {:else if $search && $search !== "?"}
+    <p>Search is {$search}</p>
+  {:else}
+    <p>There is no search term</p>
+  {/if}
+  {#if matches}
+    <p>Search currently <span class="green">matches</span></p>
+  {:else}
+    <p>Search currently <span class="red">fails</span></p>
+  {/if}
+</div>
+
+<style>
+  .container {
+    margin-top: 1rem;
+    background-color: var(--theme-card);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+  }
+  .slot {
+    display: flex;
+  }
+
+  .matches {
+    background-color: var(--theme-card-hover);
+  }
+  .exactMatch {
+    outline: 4px solid var(--theme-card-outline);
+  }
+
+  .fails {
+    display: none;
+  }
+
+  .green {
+    color: green;
+  }
+  .red {
+    color: red;
+  }
+</style>
---
+++ src/components/input.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { hideIncorrect, search } from "@components/event-store";
+  $: {
+    if (selected) {
+      const searchTerm = selected.toLowerCase();
+      search.set(searchTerm);
+    } else {
+      search.set("?");
+    }
+  }
+
+  let selected: string;
+</script>
+
+<div class="container">
+  <input type="text" bind:value={selected} placeholder="Type a name..." />
+  <div class="check">
+    <label>
+      <input type="checkbox" bind:checked={$hideIncorrect} />
+      Hide Incorrect Items
+    </label>
+  </div>
+</div>
+
+<style>
+  .container {
+    margin-top: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    place-content: center;
+    justify-content: center;
+  }
+
+  label {
+    display: flex;
+    justify-content: center;
+  }
+</style>
