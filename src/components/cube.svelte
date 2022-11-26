d1464eac026b3085421b323f7443ca404f1bb7f6
FireIsGood <fireisadequate@gmail.com>
2022-11-25 21:38:43 -0800

    Add draggable grid page

---
+++ src/components/cube.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+  export let rotateX: number = 15;
+  export let rotateY: number = 15;
+</script>
+
+<div
+  class="container"
+  style={`--rotate-x: ${rotateX}deg; --rotate-y: ${rotateY}deg;`}
+>
+  <div class="cube">
+    <span style="--index:1;">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/9/9a/Among_Us_cover_art.jpg"
+        alt=""
+      />
+    </span>
+    <span style="--index:2;">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Typical_internet_meme_image_format.svg/1280px-Typical_internet_meme_image_format.svg.png"
+        alt=""
+      />
+    </span>
+    <span style="--index:3;">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/1/18/Deep_fried_meme.jpg"
+        alt=""
+      />
+    </span>
+    <span style="--index:4;">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/6/67/Rat_diabetic.jpg"
+        alt=""
+      />
+    </span>
+    <span class="top">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/a/a5/Blaulicht_DVD_cover.jpg"
+        alt=""
+      />
+    </span>
+    <span class="bottom">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Farmer_meme_with_apostrophe.jpg/1920px-Farmer_meme_with_apostrophe.jpg"
+        alt=""
+      />
+    </span>
+  </div>
+</div>
+
+<style lang="scss">
+  .container {
+    --cube-width: 15vw;
+    width: var(--cube-width);
+    height: var(--cube-width);
+  }
+  .cube {
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+    transition: transform 150ms ease;
+    transform: rotateX(var(--rotate-x)) rotateY(var(--rotate-y));
+
+    > span {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: radial-gradient(aquamarine, black);
+
+      transform: rotateY(calc(90deg * (var(--index, 0) - 1)))
+        translateZ(calc(var(--cube-width) / 2));
+
+      > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        display: block;
+        object-fit: cover;
+        user-select: none;
+        -webkit-user-drag: none;
+      }
+
+      &.top {
+        transform: rotateX(90deg) translateZ(calc(var(--cube-width) / 2));
+      }
+      &.bottom {
+        transform: rotateX(-90deg) translateZ(calc(var(--cube-width) / 2));
+      }
+    }
+  }
+</style>
---
+++ src/components/draggable-grid.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Cube from "@components/cube.svelte";
+
+  let dragArea: Element;
+
+  let startPosX: number = 0;
+  let startPosY: number = 0;
+  let newPosX: number = 0;
+  let newPosY: number = 0;
+
+  let offsetX: number = 0;
+  let offsetY: number = 0;
+
+  let dragging: boolean = false;
+
+  const handleDrag = function (e: PointerEvent) {
+    // console.log(e?.offsetX, e?.offsetY);
+    newPosX = startPosX - e.clientX;
+    newPosY = startPosY - e.clientY;
+  };
+
+  onMount(() => {
+    dragArea.addEventListener("mousedown", (e: PointerEvent) => {
+      dragging = true;
+      startPosX = e.clientX;
+      startPosY = e.clientY;
+
+      // Handle mouse movements
+      dragArea.addEventListener("mousemove", handleDrag);
+
+      // Handle mouse up
+      dragArea.addEventListener("mouseup", () => {
+        dragArea.removeEventListener("mousemove", handleDrag);
+
+        dragging = false;
+        offsetX += newPosX;
+        offsetY += newPosY;
+        newPosX = 0;
+        newPosY = 0;
+      });
+    });
+  });
+</script>
+
+<section
+  style={`--offset-x: ${-(offsetX + newPosX)}px;
+   --offset-y: ${-(offsetY + newPosY)}px`}
+  bind:this={dragArea}
+>
+  <div class="info">
+    <p>drag: {dragging}</p>
+    <div>
+      <p>x: {newPosX}</p>
+      <p>y: {newPosY}</p>
+    </div>
+    <div>
+      <p>𐤃x: {offsetX}</p>
+      <p>𐤃y: {offsetY}</p>
+    </div>
+  </div>
+  <div class="item"><p>I'm an item!</p></div>
+  <div class="cube-area">
+    <Cube rotateY={-offsetX / 6} rotateX={offsetY / 6} />
+  </div>
+</section>
+
+<style lang="scss">
+  section {
+    position: relative;
+    display: grid;
+    height: 100vh;
+    overflow: hidden;
+    user-select: none;
+    cursor: grab;
+    background-image: radial-gradient(
+      var(--theme-card-alt) 10%,
+      transparent 10%
+    );
+    background-size: 1.25rem 1.25rem;
+
+    &:active {
+      cursor: grabbing;
+    }
+  }
+
+  .info {
+    grid-area: 1 / -1;
+    place-self: start;
+    display: grid;
+    grid-template-areas:
+      "top top"
+      "left right";
+    min-width: 20ch;
+    margin: 1ch;
+    padding: 1ch;
+    box-shadow: 2px 2px 4px 1px #0004;
+    background-color: var(--theme-card);
+
+    *:nth-child(1) {
+      grid-area: top;
+    }
+  }
+  .item {
+    position: absolute;
+    padding: 1ch;
+    background-color: var(--theme-button);
+
+    transform: translate(25ch, 4ch);
+    translate: var(--offset-x, 0) var(--offset-y, 0);
+  }
+  .cube-area {
+    grid-area: 1 / -1;
+    place-self: center;
+    position: relative;
+    width: 30vw;
+    display: grid;
+    place-content: center;
+    aspect-ratio: 1 / 1;
+  }
+</style>
